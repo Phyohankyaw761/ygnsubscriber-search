f862d3b5ef1a10ff0947a969f4dbd0d33e73820c
Phyohankyaw761 <sayargyiphyo4work@gmail.com>
2025-04-25 11:45:20 +0630

    edit script
    push

--- script.js
+++ script.js
@@ -44,13 +44,14 @@ function showResults(data) {
     card.className = 'card';
 
     card.innerHTML = `
-      <div class="field"><span>1) Account:</span> ${entry['Account'] || ''}</div>
-      <div class="field"><span>2) Scriber Name:</span> ${entry['Subscriber name'] || ''}</div>
-      <div class="field"><span>3) Phone Number:</span> ${entry['Phone number'] || ''}</div>
-      <div class="field"><span>4) Address:</span> ${entry['Installation address'] || ''}</div>
-      <div class="field"><span>5) Node Name:</span> ${entry['Subscriber node'] || ''}</div>
-      <div class="field"><span>6) ONT Serial:</span> ${entry['ONT serial'] || ''}</div>
-    `;
+    <div class="field"><span>1) Account:</span> ${entry['Account']}</div>
+    <div class="field"><span>2) Scriber Name:</span> ${entry['Subscriber name']}</div>
+    <div class="field"><span>3) Phone Number:</span> ${entry['Phone number']}</div>
+    <div class="field"><span>4) Address:</span> ${entry['Installation address']}</div>
+    <div class="field"><span>5) Node Name:</span> ${entry['Subscriber node']}</div>
+    <div class="field"><span>6) ONT Serial:</span> ${entry['ONT serial']}</div>
+  `;
+  
     container.appendChild(card);
   });
 }
